22a0a5ab909f4ed282df15b324d4ef4adb8cf2ef
skozlov <stanislav.kozlov@ecitele.com>
2020-11-17 09:30:22 +0200

    class-transformer

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import { ProjectList } from "./components/project-list";
 import { ProjectInput } from "./components/project-input";
+import { shuffle } from 'lodash';
+
+import 'reflect-metadata';
+import { plainToClass } from "class-transformer";
+
+console.log('shuffle', shuffle([1, 2, 3]));
+
 
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
+
